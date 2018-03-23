56d17a877e9d825d965d83552771cd4a2cbbbdf5
Tyson Andre <tysonandre775@hotmail.com>
2018-03-22 20:49:27 -0700

    WIP - Support union types

--- Plugin/Annotation/SymfonyAnnotationPlugin.php
+++ Plugin/Annotation/SymfonyAnnotationPlugin.php
@@ -2,7 +2,7 @@
 
 namespace Drenso\PhanExtensions\Plugin\Annotation;
 
-require_once 'Base/AnnotationPlugin.php';
+require_once __DIR__ . '/Base/AnnotationPlugin.php';
 
 use Drenso\PhanExtensions\Plugin\Annotation\Base\AnnotationPlugin;
 use Drenso\PhanExtensions\Visitor\Annotation\Base\AnnotationVisitor;
@@ -36,6 +36,7 @@ class SymfonyAnnotationVisitor extends AnnotationVisitor
 {
   /**
    * {@inheritdoc}
+   * @suppress PhanUnreferencedProtectedProperty (unused in base class?)
    */
   protected $exceptions = [
       'Annotation',

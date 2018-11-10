d53c4e6714e559c04532d0dfae1ec3f67f74ae1f
Bob van de Vijver <bob@drenso.nl>
2018-11-10 16:41:01 +0100

    Added tests for ThrowsPlugin

---
+++ tests/Plugin/DocComment/ThrowsPlugin/src/001_global_ns.php
@@ -0,0 +1,13 @@
+<?php
+
+use Test\CustomException;
+
+class Class001
+{
+  /**
+   * @throws CustomException exception
+   */
+  public function test()
+  {
+  }
+}
---
+++ tests/Plugin/DocComment/ThrowsPlugin/src/002_namespaced_class.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace MyOtherNS;
+
+use Test\CustomException;
+
+class Class002
+{
+  /**
+   * @throws CustomException exception
+   */
+  public function test()
+  {
+  }
+}

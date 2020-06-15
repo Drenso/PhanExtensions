5166829a4df781e1cb1aeb40e10eaafb16d77a4c
Bob van de Vijver <bob@drenso.nl>
2020-06-15 14:32:31 +0200

    #12: Add support for class name resolution in annotations

---
+++ tests/Plugin/Annotation/SymfonyAnnotationPlugin/src/007_class_constant_not_imported.php
@@ -0,0 +1,12 @@
+<?php
+
+use Annotations\Number1;
+
+class Class007
+{
+
+  /**
+   * @Number1(someProperty=Number2::class)
+   */
+  public $x;
+}
---
+++ tests/Plugin/Annotation/SymfonyAnnotationPlugin/src/008_class_constant.php
@@ -0,0 +1,13 @@
+<?php
+
+use Annotations\Number1;
+use Annotations\Number2;
+
+class Class008
+{
+
+  /**
+   * @Number1(someProperty=Number2::class)
+   */
+  public $x;
+}

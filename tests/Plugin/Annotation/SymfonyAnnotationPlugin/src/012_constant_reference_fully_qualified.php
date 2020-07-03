0dd7d9e195886b863add2f2f413e09e78e5d2492
Bob van de Vijver <bob@drenso.nl>
2020-07-03 13:37:25 +0200

    Accept FQN in static const reference in annotation values

---
+++ tests/Plugin/Annotation/SymfonyAnnotationPlugin/src/012_constant_reference_fully_qualified.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Test;
+
+use Annotations\Number1;
+
+class Class012
+{
+
+  /**
+   * @Number1(someProperty=Constants\Properties::TEST)
+   */
+  public $x;
+}
---
+++ tests/Plugin/Annotation/SymfonyAnnotationPlugin/src/013_constant_reference.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Some\Name\Space;
+
+use Annotations\Number1;
+use Constants\Properties;
+
+class Class014
+{
+
+  /**
+   * @Number1(someProperty=Properties::TEST)
+   */
+  public $x;
+}
